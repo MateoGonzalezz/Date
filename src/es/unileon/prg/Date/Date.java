1e1360f4096ab1d5c4f07fd1c80b6e80c3c693ea
MateoGonzalezz <mgonza40@estudiantes.unileon.es>
2021-11-11 11:24:04 +0100

    11/11_1123

--- src/es/unileon/prg/Date/Date.java
+++ src/es/unileon/prg/Date/Date.java
@@ -1,5 +1,8 @@
-
 package es.unileon.prg.Date;
+
+import java.time.DayOfWeek;
+
+import javax.sql.rowset.spi.SyncResolver;
 
 public class Date {
 
@@ -9,28 +12,27 @@ public class Date {
 	
 	
 	public int getDay(){
-		return day;
+		return this.day;
 	}
 	public int getMonth(){
-		return month;
+		return this.month;
 	}
 	public int getYear(){
-		return year;
+		return this.year;
 	}
 
 
 	public Date(int day,int month, int year) {
-		this.setDay(day);
-		this.setMonth(month);
-		this.setYear(year);
+		this.day =day;
+		this.month =month;
+		this.year=year;
 		
 	}
 	
 	
-	public int getDayOfMonth() {
-		
+	public int getDayOfMonth(int monthdays) {
 		int numdias = 0;
-		switch(month) {
+		switch(monthdays) {
 		case 1:
 		case 3:
 		case 5:
@@ -55,8 +57,11 @@ public class Date {
 	
 	
 	public void setDay(int day) {
-		if (day>1||day<this.getDayOfMonth()) {
+		if (day>1||day<this.getDayOfMonth(this.month)) {
 			this.day=day;
+		}
+		else{
+			throw
 		}
 	}
 	boolean isSameDay(Date otrodia) {
@@ -97,10 +102,214 @@ public class Date {
 	}
 	
 
-	boolean void isSame( Date igual){
-		if (this.day)
+	boolean isSame( Date igualdia,Date igualmes, Date igualano){
+		boolean issame=false;
+		if ((this.day == igualdia.day)&&(this.month == igualmes.month)&&(this.year == igualdia.year)){
+		 issame=true;	
+		}
+		return issame;
 	}
 	
+	public String nombreMes(int nombremes){
+		String mes= "";
+		this.month=nombremes;
+		switch(nombremes){
+			case 1:
+			mes = "Enero";
+			break;
+			case 2:
+			mes = "Febrero";
+			break;
+			case 3:
+			mes = "Marzo";
+			break;
+			case 4:
+			mes = "Abril";
+			break;
+			case 5:
+			mes = "Mayo";
+			break;
+			case 6:
+			mes = "Junio";
+			break;
+			case 7:
+			mes = "Julio";
+			break;
+			case 8:
+			mes = "Agosto";
+			break;
+			case 9:
+			mes = "Septiembre";
+			break;
+			case 10:
+			mes = "Octubre";
+			break;
+			case 11:
+			mes = "Noviembre";
+			break;
+			case 12:
+			mes = "Diciembre";
+			break;
+		}
+			return mes;
+	}
+
+	public boolean comprobarDia(){
+		boolean diacorrecto = true;
+		if(day<1||day>this.getDayOfMonth(this.month)){
+			diacorrecto = false;
+		}
+		return diacorrecto;
+	}
+	public String nombreEstacion (Date nombreestacion) {
+		String estacion = "";
+		this.month = nombreestacion.month;
+
+		switch (nombreestacion.month){ 
+			case 1:
+			case 2:
+				estacion="Invierno";
+				break;
+			case 3:
+			if (day<21){
+				estacion="Invierno";
+			}
+			else {
+				estacion="Primavera";
+			}
+			break;
+			case 4:
+			case 5:
+				estacion ="Primavera";
+				break;
+			case 6:
+			if (day<21){
+				estacion="Primavera";
+			}
+			else {
+				estacion="Verano";
+			}
+			break;
+			case 7:
+			case 8:
+				estacion = "Verano";
+				break;
+			case 9:
+			if (day<21){
+				estacion="Verano";
+			}
+			else {
+				estacion="Otono";
+			}
+			break;
+			case 10:
+			case 11:
+				estacion = "Otono";
+				break;
+			case 12:
+			if (day<21){
+				estacion="Otono";
+			}
+			else {
+				estacion="Invierno";
+			}
+			break;
+		}
+		return estacion;
+	}
+
+	public int mesesfaltan(Date mesesfaltan){
+		int mesesrestantes= 12 - mesesfaltan.month;
+		return mesesrestantes;
+	}	
 	
+	public String toStringLet(){
+		return day +" de "+ this.nombreMes(this.month) +" del año "+ year;
+	}
+
+	public String toStringNum(){
+		return day +"/"+ month +"/"+ year;
+	}
+
+	public String datesUntilEnd(){
+		StringBuilder cadena = new StringBuilder();
+		for (int i=day;i <= this.getDayOfMonth(this.month);i++){
+		cadena.append(i+"/"+this.month +"/"+ this.year + ", " );
+		}
+	return cadena.toString();
+	}
+
+	public String monthsSameNumber(Date diasmes){
+		
+		StringBuilder cadena = new StringBuilder();
+		for(int i = 1; i<=12;i++){
+			if(getDayOfMonth(i) == getDayOfMonth(diasmes.month)){
+				cadena.append(String.valueOf(this.nombreMes(i)) + " ");
+			}
+		}
+		return cadena.toString();
 	
+	}
+
+	public int daysSinceStart(){
+		int sumadias = 0;
+		for (int i=1;i < this.month;i++){
+			sumadias= sumadias + getDayOfMonth(i)+ this.day;
+		}
+		return sumadias;
+	}
+
+
+	//Esta bien solo que tarda en hacerla coincidir
+	/*
+	public int dateTry(){
+		int cont=0;	
+		double dayrandom = 0;
+		double monthrandom = 0;
+		do{
+			dayrandom = Math.random()*getDayOfMonth(this.month)+1;
+			monthrandom = Math.random()*12 + 1;
+			cont++;
+			i++;
+			System.out.println("Intento "+ i);
+		}while ((dayrandom!=this.day)||(monthrandom!=this.month));
+		return cont;
+	}
+	*/
+	public String nameDayOfWeek(int weekday){
+		String weekdayname = "";
+		switch(weekday){
+			case 0:
+				weekdayname = "Lunes";
+				break;
+			case 1:
+				weekdayname = "Martes";
+				break;
+			case 2:
+				weekdayname = "Miercoles";
+				break;
+			case 3:
+				weekdayname = "Jueves";
+				break;
+			case 4:
+				weekdayname = "Viernes";
+				break;
+			case 5:
+				weekdayname = "Sabado";
+				break;
+			case 6:
+				weekdayname = "Domingo";
+				break;
+		}
+		return weekdayname;
+	}
+
+	public String dayOfWeek(int firstday){
+		int aux = 0;
+		String cadena = "";
+		aux = (firstday%7 + this.daysSinceStart())%7;
+		cadena = this.nameDayOfWeek(aux);
+
+		return cadena;
+	}
 }
--- src/es/unileon/prg/Date/Maindate.java
+++ src/es/unileon/prg/Date/Maindate.java
@@ -5,15 +5,35 @@ public class Maindate {
 	public static void main (String [] args) {
 		Date today, tomorrow;
 		
-		today= new Date(10,10,2020);
-		tomorrow = new Date (12,12,2020);
+		today= new Date(24,3,2022);
+		tomorrow = new Date (12,1,2020);
+		
 
-		System.out.println("El dia es (igual.true  distinto-false)" + today.isSameDay(tomorrow));
-		System.out.println("Dia today: " + today.getDay() +" Dia tomorrow: "+ tomorrow.getDay());
-		System.out.println("El mes es (igual.true  distinto-false)" + today.isSameMonth(tomorrow));
-		System.out.println("Mes today: " + today.getMonth() +" Mes tomorrow: "+ tomorrow.getMonth());
-		System.out.println("El ano es (igual.true  distinto-false)" + today.isSameYear(tomorrow));
-		System.out.println("Año today: " + today.getYear() +" Año tomorrow: "+ tomorrow.getYear());
+		System.out.println("El dia es igual: " + today.isSameDay(tomorrow));
+		System.out.println("Dia today: " + today.getDay() +"   Dia tomorrow: "+ tomorrow.getDay());
+		System.out.println("El mes es igual: " + today.isSameMonth(tomorrow));
+		System.out.println("Mes today: " + today.getMonth() +"   Mes tomorrow: "+ tomorrow.getMonth());
+		System.out.println("El ano es igual: " + today.isSameYear(tomorrow));
+		System.out.println("Año today: " + today.getYear() +"   Año tomorrow: "+ tomorrow.getYear());
+		System.out.println("El dia mes y año son iguales:  " + today.isSame(tomorrow,tomorrow,tomorrow));
+		System.out.println("Mes today: " + today.nombreMes(today.getMonth()));
+		System.out.println("Mes tomorow: " + tomorrow.nombreMes(tomorrow.getMonth()));
+		System.out.println("La fecha es: " + today.getDay() +"/"+ today.getMonth() +"/"+ today.getYear()+"    "+ today.getDay()+ " de "+ today.nombreMes(today.getMonth()) + " del año "+ today.getYear());
+		System.out.println("La fecha es: " + tomorrow.getDay() +"/"+ tomorrow.getMonth() +"/"+ tomorrow.getYear()+"    "+ tomorrow.getDay()+ " de "+ tomorrow.nombreMes(tomorrow.getMonth()) + " del año "+ tomorrow.getYear());
+		System.out.println("La estación de today es: " + today.nombreEstacion(today));
+		System.out.println("La estación de tomorrow es: " + tomorrow.nombreEstacion(tomorrow));
+		System.out.println("El dia del mes de today es correcto: " + today.comprobarDia());
+		System.out.println("El dia del mes de tomorrow es correcto: " + tomorrow.comprobarDia());
+		System.out.println("Faltan " + today.mesesfaltan(today) + " meses para que se acabe el año");
+		System.out.println("Faltan " + tomorrow.mesesfaltan(tomorrow) + " meses para que se acabe el año");
+		System.out.println(today.toStringLet()+"   "+today.toStringNum());
+		System.out.println(tomorrow.toStringLet()+"   "+tomorrow.toStringNum());
+		System.out.println(today.datesUntilEnd() + " fechas hasta el final de mes");
+		System.out.println(tomorrow.monthsSameNumber(tomorrow));
+		System.out.println(today.daysSinceStart() + " dias desde el principio de año");
+	//	System.out.println(today.dateTry());
+		System.out.println(today.dayOfWeek(5));
+
 
 	}
 }
